74599c9a7348a4d2995894aef85a0c6aa4ffff57
tenma-nakano823 <t.nakano.806@ms.saitama-u.ac.jp>
2024-06-11 04:28:05 +0900

    migration & breeze実装

---
+++ app/Http/Controllers/MemberEventPaidController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MemberEventPaidController extends Controller
+{
+    //
+}
---
+++ app/Models/MemberEventPaid.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MemberEventPaid extends Model
+{
+    use HasFactory;
+    
+    public function member()   
+    {
+        return $this->belongsTo(Member::class);  
+    }
+    
+    public function event()   
+    {
+        return $this->belongsTo(Event::class);  
+    }
+}
